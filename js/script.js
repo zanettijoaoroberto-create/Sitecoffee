63fd496b38fbaf759fb555ea6113425141171fea
isis-ruiz <isisr5281@gmail.com>
2026-06-16 17:01:12 -0300

    Site da cafeteria

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// MENSAGEM DE CADASTRO
+
+if (document.getElementById("btnCadastro")) {
+
+    document.getElementById("btnCadastro").onclick = function () {
+
+        document.getElementById("mensagem").innerHTML =
+        "Cadastro realizado com sucesso!";
+
+    }
+
+}
+
+// MENSAGEM DE RECUPERAÇÃO
+
+if (document.getElementById("btnRecuperar")) {
+
+    document.getElementById("btnRecuperar").onclick = function () {
+
+        document.getElementById("mensagemRecuperar").innerHTML =
+        "Senha alterada com sucesso!";
+
+    }
+
+}
+// ENVIO DE MENSAGEM DE CADASTRO
+if (document.getElementById("btnCadastro")) {
+
+    document.getElementById("btnCadastro").onclick = function () {
+
+        document.getElementById("mensagem").innerHTML =
+        "Cadastro realizado com sucesso!";
+
+    }
+
+} 
+// ENVIO DE MENSAGEM DE RECUPERAÇÃO
+
+if (document.getElementById("btnRecuperar")) {
+
+    document.getElementById("btnRecuperar").onclick = function () {
+
+        document.getElementById("mensagemRecuperar").innerHTML =
+        "Senha alterada com sucesso!";
+
+    }
+
+}
